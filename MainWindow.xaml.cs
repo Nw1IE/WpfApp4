e8c5bf2a8ac0acc20ddfe41fb0aaf2ad22c476d0
ElNefro <Yanik0206@gmail.com>
2025-05-28 15:00:44 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ToDo> todo = new List<ToDo>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void btnAdd(object sender, RoutedEventArgs e)
+        {
+            var newToDo = new NewToDo();
+            newToDo.Show();
+            newToDo.Owner = this;
+
+        }
+
+
+        private void btnDelete(object sender, RoutedEventArgs e)
+        {
+            if (ToDolist.SelectedItem as ToDo == null)
+            {
+                return;
+            }
+            else
+            {
+                todo.Remove((ToDo)ToDolist.SelectedItem);
+
+                ToDolist.ItemsSource = null;
+                ToDolist.ItemsSource = todo;
+            }
+        }
+
+
+        public class ToDo
+        {
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+            public string Description { get; set; }
+
+            public ToDo(string name, DateTime date, string description)
+            {
+                Name = name;
+                Date = date;
+                Description = description;
+            }
+        }
+    }
+}
---
+++ NewToDo.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static WpfApp4.MainWindow;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для NewToDo.xaml
+    /// </summary>
+    public partial class NewToDo : Window
+    {
+        public NewToDo()
+        {
+            InitializeComponent();
+
+            dateToDo.SelectedDate = DateTime.Now;
+        }
+
+
+        private void ToDoSave(object sender, RoutedEventArgs e)
+        {
+            if (dateToDo.SelectedDate == null)
+            {
+                MessageBox.Show("Выберите дату!");
+                return;
+            }
+            var main = (MainWindow)Owner;
+
+            var temp = dateToDo.SelectedDate.Value;
+            main.todo.Add(new ToDo(titleToDo.Text, temp, descriptionToDo.Text));
+
+            main.ToDolist.ItemsSource = null;
+            main.ToDolist.ItemsSource = main.todo;
+
+            Hide();
+        }
+    }
+}
+
