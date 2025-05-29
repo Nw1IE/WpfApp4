2dcb7c18a83732dc378dcd4bdf0240dc40293364
ElNefro <Yanik0206@gmail.com>
2025-05-29 14:44:26 +0500

    bew

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics.PerformanceData;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,6 +19,15 @@ namespace WpfApp4
     public partial class MainWindow : Window
     {
         public List<ToDo> todo = new List<ToDo>();
+        public ObservableCollection<ToDo> ToDoList = new ObservableCollection<ToDo>();
+        public int TodoListCount
+        {
+            get => ToDoList.Count;
+        }
+        public int ToDoListCountComplited
+        {
+            get => ToDoList.Where(p => p.Doing == true).Count();
+        }
 
         public MainWindow()
         {
@@ -49,18 +60,35 @@ namespace WpfApp4
             }
         }
 
+        private void UpdateCounters()
+        {
+            CounterText.Text = $"{ToDoList.Count}/{ToDoList.Count(t => t.Doing)}";
+            Progress.Maximum = ToDoList.Count;
+            Progress.Value = ToDoList.Count(t => t.Doing);
+        }
+
+        private void EndToDo(object sender, RoutedEventArgs e)
+        {
+          
+        }
 
         public class ToDo
         {
+            private bool doing_;
             public string Name { get; set; }
             public DateTime Date { get; set; }
             public string Description { get; set; }
-
+            public bool Doing
+            {
+                get => doing_;
+                set => doing_ = value;
+            }
             public ToDo(string name, DateTime date, string description)
             {
                 Name = name;
                 Date = date;
                 Description = description;
+                doing_ = false;
             }
         }
     }
